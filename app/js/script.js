566871808294c8968fa38fc3d868c45dc2352a84
Maritim Daniel <maritimdaniel07@gmai.com>
2023-03-14 15:49:13 +0300

    add sticy navigation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,4 @@
-console.log("hello from personal blog");
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
