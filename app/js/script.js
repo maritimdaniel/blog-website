ae2bc6731c8fa66510e9d58100da73ae686a6bc1
Maritim Daniel <maritimdaniel07@gmai.com>
2023-03-15 17:16:26 +0300

    add cookie section

--- app/js/script.js
+++ app/js/script.js
@@ -2,3 +2,12 @@ window.addEventListener("scroll", () => {
   const header = document.querySelector(".header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+const cookie = document.querySelector(".cookie");
+const cookieBtns = document.querySelectorAll(".cookie-btn");
+
+cookieBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    cookie.style.display = "none";
+  });
+});
